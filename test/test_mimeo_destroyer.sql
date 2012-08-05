a620f96fcf0e1052238dc12f0d71645db01a8166
Keith Fiske <keith@keithf4.com>
2012-08-05 18:15:55 -0400

    Make destroyer test function a little smarter to make test cleanup easier if things don't run properly

--- test/test_mimeo_destroyer.sql
+++ test/test_mimeo_destroyer.sql
@@ -3,6 +3,7 @@ CREATE OR REPLACE FUNCTION test_mimeo_destroyer (p_archive text) RETURNS void
     AS $$
 DECLARE
 
+v_check             int;
 v_mimeo_schema      text;
 v_old_search_path   text;
 
@@ -12,14 +13,32 @@ SELECT current_setting('search_path') INTO v_old_search_path;
 SELECT nspname INTO v_mimeo_schema FROM pg_namespace n, pg_extension e WHERE e.extname = 'mimeo' AND e.extnamespace = n.oid;
 EXECUTE 'SELECT set_config(''search_path'','''||v_mimeo_schema||''',''false'')';
 
-PERFORM snapshot_destroyer('mimeo_source.snap_test_source', p_archive);
-PERFORM snapshot_destroyer('mimeo_dest.snap_test_dest', p_archive);
+SELECT count(*) INTO v_check FROM refresh_config WHERE dest_table = 'mimeo_source.snap_test_source';
+IF v_check >= 1 THEN
+    PERFORM snapshot_destroyer('mimeo_source.snap_test_source', p_archive);
+END IF;
+SELECT count(*) INTO v_check FROM refresh_config WHERE dest_table = 'mimeo_source.snap_test_dest';
+IF v_check >= 1 THEN
+    PERFORM snapshot_destroyer('mimeo_dest.snap_test_dest', p_archive);
+END IF;
 
-PERFORM inserter_destroyer('mimeo_source.inserter_test_source', p_archive);
-PERFORM inserter_destroyer('mimeo_dest.inserter_test_dest', p_archive);
+SELECT count(*) INTO v_check FROM refresh_config WHERE dest_table = 'mimeo_source.inserter_test_source';
+IF v_check >= 1 THEN
+    PERFORM inserter_destroyer('mimeo_source.inserter_test_source', p_archive);
+END IF;
+SELECT count(*) INTO v_check FROM refresh_config WHERE dest_table = 'mimeo_source.inserter_test_dest';
+IF v_check >= 1 THEN
+    PERFORM inserter_destroyer('mimeo_dest.inserter_test_dest', p_archive);
+END IF;
 
-PERFORM updater_destroyer('mimeo_source.updater_test_source', p_archive);
-PERFORM updater_destroyer('mimeo_dest.updater_test_dest', p_archive);
+SELECT count(*) INTO v_check FROM refresh_config WHERE dest_table = 'mimeo_source.updater_test_source';
+IF v_check >= 1 THEN
+    PERFORM updater_destroyer('mimeo_source.updater_test_source', p_archive);
+END IF;
+SELECT count(*) INTO v_check FROM refresh_config WHERE dest_table = 'mimeo_source.updater_test_dest';
+IF v_check >= 1 THEN
+    PERFORM updater_destroyer('mimeo_dest.updater_test_dest', p_archive);
+END IF;
 
 EXECUTE 'SELECT set_config(''search_path'','''||v_old_search_path||''',''false'')';
 
