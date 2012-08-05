20472b5973b749e073e22a2490375d744e3e2cce
Keith Fiske <keith@keithf4.com>
2012-08-05 18:03:40 -0400

    Fix missing inserter_maker functions

--- sql/mimeo.sql
+++ sql/mimeo.sql
@@ -1409,6 +1409,152 @@ $$;
 
 
 /*
+ *  Inserter maker function. Assumes source and destination are the same tablename.
+ */
+CREATE OR REPLACE FUNCTION inserter_maker(p_src_table text, p_control_field text, p_dblink_id int, p_boundary interval DEFAULT '00:10:00') RETURNS void
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+
+v_data_source               text;
+v_dst_active                boolean;
+v_exists                    int;
+v_insert_refresh_config     text;
+v_max_timestamp             timestamptz;
+v_snap_suffix               text;
+v_view_definition           text;
+
+BEGIN
+
+SELECT data_source INTO v_data_source FROM @extschema@.dblink_mapping WHERE data_source_id = p_dblink_id; 
+IF NOT FOUND THEN
+	RAISE EXCEPTION 'ERROR: database link ID is incorrect %', p_dblink_id; 
+END IF;  
+
+-- Temp snap config
+v_insert_refresh_config := 'INSERT INTO @extschema@.refresh_config_snap(source_table, dest_table, dblink) VALUES('
+    ||quote_literal(p_src_table)||', '||quote_literal(p_src_table)||', '|| p_dblink_id||')';
+
+RAISE NOTICE 'Inserting record in @extschema@.refresh_config';
+EXECUTE v_insert_refresh_config;	
+RAISE NOTICE 'Insert successful';	
+
+RAISE NOTICE 'attempting snapshot';
+PERFORM @extschema@.refresh_snap(p_src_table, FALSE);
+
+RAISE NOTICE 'attempting to destroy snapshot';
+
+SELECT definition INTO v_view_definition FROM pg_views WHERE schemaname || '.' || viewname = p_src_table;
+	v_exists := strpos(v_view_definition, 'snap1');
+	IF v_exists > 0 THEN
+    	v_snap_suffix := 'snap1';
+	END IF;
+
+	EXECUTE 'DROP VIEW ' || p_src_table;
+	EXECUTE 'CREATE TABLE ' || p_src_table || ' AS SELECT * FROM ' || p_src_table || '_' || v_snap_suffix;
+EXECUTE 'DROP TABLE ' || p_src_table || '_snap1';
+
+RAISE NOTICE 'Destroyed successfully';
+
+RAISE NOTICE 'Taking the maximum timestamp';
+EXECUTE 'SELECT max('||p_control_field||') FROM '||p_src_table||';' INTO v_max_timestamp;
+RAISE NOTICE 'The select statement ran successfully.';
+
+v_dst_active := @extschema@.dst_utc_check();
+
+v_insert_refresh_config := 'INSERT INTO @extschema@.refresh_config_inserter(source_table, dest_table, dblink, control, boundary, last_value, dst_active) VALUES('
+    ||quote_literal(p_src_table)||', '||quote_literal(p_src_table)||', '|| p_dblink_id||', '
+    ||quote_literal(p_control_field)||', '''||p_boundary||'''::interval,'''||v_max_timestamp||'''::timestamptz, '||v_dst_active||');';
+
+RAISE NOTICE 'Updating config table with highest timestamp value';
+EXECUTE v_insert_refresh_config;
+RAISE NOTICE 'Update successful';
+
+-- Remove temp snap from config
+EXECUTE 'DELETE FROM @extschema@.refresh_config_snap WHERE source_table = '||quote_literal(p_src_table)||' AND dest_table = '||quote_literal(p_src_table);
+
+RAISE NOTICE 'all done';
+
+RETURN;
+
+END
+$$;
+
+
+/*
+ *  Inserter maker function. Accepts custom destination name.
+ */
+CREATE OR REPLACE FUNCTION inserter_maker(p_src_table text, p_dest_table text, p_control_field text, p_dblink_id int, p_boundary interval DEFAULT '00:10:00') RETURNS void
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+
+v_data_source               text;
+v_dst_active                boolean;
+v_exists                    int;
+v_insert_refresh_config     text;
+v_max_timestamp             timestamptz;
+v_snap_suffix               text;
+v_view_definition           text;
+
+BEGIN
+
+SELECT data_source INTO v_data_source FROM @extschema@.dblink_mapping WHERE data_source_id = p_dblink_id; 
+IF NOT FOUND THEN
+	RAISE EXCEPTION 'ERROR: database link ID is incorrect %', p_dblink_id; 
+END IF;  
+
+-- Temp snap config
+v_insert_refresh_config := 'INSERT INTO @extschema@.refresh_config_snap(source_table, dest_table, dblink) VALUES('
+    ||quote_literal(p_src_table)||', '||quote_literal(p_dest_table)||', '|| p_dblink_id||')';
+
+RAISE NOTICE 'Inserting record in @extschema@.refresh_config';
+EXECUTE v_insert_refresh_config;	
+RAISE NOTICE 'Insert successful';	
+
+RAISE NOTICE 'attempting snapshot';
+PERFORM @extschema@.refresh_snap(p_dest_table, FALSE);
+
+RAISE NOTICE 'attempting to destroy snapshot';
+
+SELECT definition INTO v_view_definition FROM pg_views WHERE schemaname || '.' || viewname = p_dest_table;
+	v_exists := strpos(v_view_definition, 'snap1');
+	IF v_exists > 0 THEN
+    	v_snap_suffix := 'snap1';
+	END IF;
+
+	EXECUTE 'DROP VIEW ' || p_dest_table;
+	EXECUTE 'CREATE TABLE ' || p_dest_table || ' AS SELECT * FROM ' || p_dest_table || '_' || v_snap_suffix;
+EXECUTE 'DROP TABLE ' || p_dest_table || '_snap1';
+
+RAISE NOTICE 'Destroyed successfully';
+
+RAISE NOTICE 'Taking the maximum timestamp';
+EXECUTE 'SELECT max('||p_control_field||') FROM '||p_dest_table||';' INTO v_max_timestamp;
+RAISE NOTICE 'The select statement ran successfully.';
+
+v_dst_active := @extschema@.dst_utc_check();
+
+v_insert_refresh_config := 'INSERT INTO @extschema@.refresh_config_inserter(source_table, dest_table, dblink, control, boundary, last_value, dst_active) VALUES('
+    ||quote_literal(p_src_table)||', '||quote_literal(p_dest_table)||', '|| p_dblink_id||', '
+    ||quote_literal(p_control_field)||', '''||p_boundary||'''::interval, '''||v_max_timestamp||'''::timestamptz, '||v_dst_active||');';
+
+RAISE NOTICE 'Updating config table with highest timestamp value';
+EXECUTE v_insert_refresh_config;
+RAISE NOTICE 'Update successful';
+
+-- Remove temp snap from config
+EXECUTE 'DELETE FROM @extschema@.refresh_config_snap WHERE source_table = '||quote_literal(p_src_table)||' AND dest_table = '||quote_literal(p_dest_table);
+
+RAISE NOTICE 'all done';
+
+RETURN;
+
+END
+$$;
+
+
+/*
  *  Inserter destroyer function. Pass archive to keep table intact.
  */
 CREATE FUNCTION inserter_destroyer(p_dest_table text, p_archive_option text) RETURNS void
